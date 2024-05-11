01eb7f7d2a5ca182536a81f5fb2a3317b137316c
lorenzolarghi <elel.develop@gmail.com>
2024-05-11 20:00:34 +0200

    feat: trying export

--- src/index.ts
+++ src/index.ts
@@ -21,4 +21,4 @@ const ssplit = (toSplit: string, chars: string[], indexes: number[]) => {
   return recursiveDivide(toSplit, chars, indexes);
 };
 
-export default ssplit;
+module.exports = ssplit;
